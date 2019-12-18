af5f3c7f3083ba3658c5194f67e7da061eb78bdc
Z <zacharyk.ishmael@gmail.com>
2019-12-18 16:29:34 +0100

    Update Clazz.java
    Clazz:
    Added javax persistence annotations

--- src/main/java/app/CHEditor/domain/Clazz.java
+++ src/main/java/app/CHEditor/domain/Clazz.java
@@ -1,9 +1,21 @@
 package app.CHEditor.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Clazz {
+	@Column
 	int pid;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	int cid;
+	@Column
 	String name;
+	@Column
 	boolean abstract_;
 	
 	public Clazz () {
