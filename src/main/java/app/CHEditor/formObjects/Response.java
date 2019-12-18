fcbefba8f8609f3392b5f0d5aaf20b4ad07ada89
Z <zacharyk.ishmael@gmail.com>
2019-12-18 17:25:25 +0100

    Response.java and CHEditorRest.java added

---
+++ src/main/java/app/CHEditor/formObjects/Response.java
@@ -0,0 +1,32 @@
+package app.CHEditor.formObjects;
+
+public class Response {
+
+	boolean response;
+	String message;
+	/**
+	 * @return the response
+	 */
+	public boolean isResponse() {
+		return response;
+	}
+	/**
+	 * @param response the response to set
+	 */
+	public void setResponse(boolean response) {
+		this.response = response;
+	}
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
---
+++ src/main/java/app/CHEditor/rest/CHEditorRest.java
@@ -0,0 +1,33 @@
+package app.CHEditor.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import app.CHEditor.formObjects.Response;
+
+@RestController
+public class CHEditorRest {
+	
+	public Response createClass() {
+		Response res = new Response();
+		
+		return res;
+	}
+	
+	public Response readClass() {
+		Response res = new Response();
+		
+		return res;
+	}
+	
+	public Response updateClass() {
+		Response res = new Response();
+		
+		return res;
+	}
+	
+	public Response deleteClass() {
+		Response res = new Response();
+		
+		return res;
+	}
+}
