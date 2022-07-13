896c48770116bd366a87d119e3740e9b8fd558d2
ZIshmael <zacharyk.ishmael@gmail.com>
2022-07-13 18:31:27 +0200

    Prepared project for heroku deploymeny

--- src/main/java/app/CHEditor/CHEditorApplication.java
+++ src/main/java/app/CHEditor/CHEditorApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class CHEditorApplication {
+public class CHEditorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CHEditorApplication.class, args);
